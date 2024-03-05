39157a1ec93d49df585b661a3663e6274d322fd1
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2024-03-04 22:19:34 -0300

    Classe Funcionario - Aula 02
    Criação da classe Funcionario
    Criação da classe Data

--- Aula02TPI/src/aula02TPI/Data.java
+++ Aula02TPI/src/aula02TPI/Data.java
@@ -1,5 +1,23 @@
 package aula02TPI;
 
 public class Data {
-    
+    public int dia;
+    public int mes;
+    public int ano;
+
+    public void defineDia(int d) {
+        this.dia = d;
+    }
+
+    public void defineMes(int m) {
+        this.mes = m;
+    }
+
+    public void defineAno(int a) {
+        this.ano = a;
+    }
+
+    public String exibeData() {
+        return this.dia + "/" + this.mes + "/" + this.ano;
+    }
 }
